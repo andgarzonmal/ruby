7d7a1b9189e94c381a21ee872e8169826bb0ab9f
andgarzonmal <andgarzonmal@unal.edu.co>
2022-11-15 10:58:31 -0500

    Refactor

--- auth.rb
+++ auth.rb
@@ -3,16 +3,7 @@ require 'bcrypt'
 def create_hash_digest(password)
   BCrypt::Password.create(password)
 end
+new_password = create_hash_digest("password")
 
-my_password = BCrypt::Password.create("my password")
-#=> "$2a$12$K0ByB.6YI2/OYrB4fQOYLe6Tv0datUVf6VZ/2Jzwm879BW5K1cHey"
+puts new password
 
-puts my_password
-puts my_password.version              #=> "2a"
-puts my_password.cost                 #=> 12
-puts my_password == "my password"     #=> true
-puts my_password == "not my password" #=> false
-
-# my_password = BCrypt::Password.new("$2a$12$K0ByB.6YI2/OYrB4fQOYLe6Tv0datUVf6VZ/2Jzwm879BW5K1cHey")
-# my_password == "my password"     #=> true
-# my_password == "not my password" #=> false
