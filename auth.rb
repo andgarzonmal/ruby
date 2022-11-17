d665411da2e5b689294d703076e4585fb3a69a11
andgarzonmal <andgarzonmal@unal.edu.co>
2022-11-17 12:31:33 -0500

    Refactor code

--- auth.rb
+++ auth.rb
@@ -1,36 +1,29 @@
 require 'bcrypt'
 
-users = [
-          { username: "mashrur", password: "password1" },
-          { username: "jack", password: "password2" },
-          { username: "arya", password: "password3" },
-          { username: "jonshow", password: "password4" },
-          { username: "heisenberg", password: "password5" }
-        ]
-
-def create_hash_digest(password)
-  BCrypt::Password.create(password)
-end
-
-def verify_hash_digest(password)
-  BCrypt::Password.new(password)
-end
-
-def create_secure_users(list_of_users)
-  list_of_users.each do |user_record|
-    user_record[:password] = create_hash_digest(user_record[:password])
+module Crud
+  puts "Module CRUD activated"
+ 
+  def Crud.create_hash_digest(password)
+    BCrypt::Password.create(password)
   end
-  list_of_users
-end
-
-def authenticate_user(username, password, list_of_users)
-  list_of_users.each do |user_record|
-    if user_record[:username] == username && verify_hash_digest(user_record[:password]) == password
-      return user_record
+ 
+  def Crud.verify_hash_digest(password)
+    BCrypt::Password.new(password)
+  end
+ 
+  def Crud.create_secure_users(list_of_users)
+    list_of_users.each do |user_record|
+      user_record[:password] = create_hash_digest(user_record[:password])
     end
-    "Credentials where not correct"
+    list_of_users
+  end
+ 
+  def Crud.authenticate_user(username, password, list_of_users)
+    list_of_users.each do |user_record|
+      if user_record[:username] == username && verify_hash_digest(user_record[:password]) == password
+        return user_record
+      end
+    end
+    "Credentials were not correct"
   end
 end
-
-authenticate_user("heisenberg", "password5", users)
-
